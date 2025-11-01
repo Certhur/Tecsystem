3b9e37ab6c42dad53836494c50b0b6019b799492
Certhur <ivan.certhursamu@gmail.com>
2025-10-31 21:15:28 -0300

    producto_fase_1

--- modules/producto/proses.php
+++ modules/producto/proses.php
@@ -7,13 +7,15 @@ if(empty($_SESSION["username"]) && empty($_SESSION["password"])){
 else {
     if($_GET["act"]=="insert"){
         if(isset($_POST["Guardar"])){
-            $cod_producto = $_POST["cod_producto"];
-            $cod_tipo_prod = $_POST["cod_tipo_prod"];
+            $id_producto = $_POST["id_producto"];
+            $id_marca = $_POST["id_marca"];
             $id_u_medida = $_POST["id_u_medida"];
             $p_descrip = $_POST["p_descrip"];
-            $precio = $_POST["precio"];
-            $query = mysqli_query($mysqli, "INSERT INTO producto(cod_producto,cod_tipo_prod,id_u_medida,p_descrip,precio)
-            VALUES ($cod_producto,'$cod_tipo_prod','$id_u_medida','$p_descrip','$precio');") or die('Error'.mysqli_error($mysqli));
+            $p_precio_servicio = $_POST["p_precio_servicio"];
+            $p_costo_actual = $_POST["p_costo_actual"];
+            $cod_proveedor = $_POST["cod_proveedor"];
+            $query = mysqli_query($mysqli, "INSERT INTO producto(id_producto,id_marca,id_u_medida,p_descrip,p_precio_servicio)
+            VALUES ($id_producto,'$id_marca','$id_u_medida','$p_descrip','$p_precio_servicio');") or die('Error'.mysqli_error($mysqli));
             if($query){
                 header("Location: ../../main.php?module=producto&alert=1");
             } else {
@@ -23,18 +25,18 @@ else {
     }
     elseif($_GET["act"]=="update"){
         if(isset($_POST["Guardar"])){
-            if(isset($_POST["cod_producto"])){
-                $cod_producto = $_POST["cod_producto"];
-                $cod_tipo_prod = $_POST["cod_tipo_prod"];
+            if(isset($_POST["id_producto"])){
+                $id_producto = $_POST["id_producto"];
+                $id_marca = $_POST["id_marca"];
                 $id_u_medida = $_POST["id_u_medida"];
                 $p_descrip = $_POST["p_descrip"];
-                $precio = $_POST["precio"];
+                $p_precio_servicio = $_POST["p_precio_servicio"];
                 $query = mysqli_query($mysqli, "UPDATE producto SET 
-                cod_tipo_prod = '$cod_tipo_prod',
+                id_marca = '$id_marca',
                 id_u_medida = '$id_u_medida',
                 p_descrip = '$p_descrip',
-                precio = '$precio' 
-                WHERE cod_producto = $cod_producto;")
+                p_precio_servicio = '$p_precio_servicio' 
+                WHERE id_producto = $id_producto;")
                 or die('Error'.mysqli_error($mysqli));
                 if($query){
                 header("Location: ../../main.php?module=producto&alert=2");
@@ -45,9 +47,9 @@ else {
         }
     }
     elseif($_GET["act"]=="delete"){
-        if(isset($_GET["cod_producto"])){
-            $cod_producto = $_GET["cod_producto"];
-            $query = mysqli_query($mysqli, "DELETE FROM producto WHERE cod_producto = $cod_producto;")
+        if(isset($_GET["id_producto"])){
+            $id_producto = $_GET["id_producto"];
+            $query = mysqli_query($mysqli, "DELETE FROM producto WHERE id_producto = $id_producto;")
             or die('Error'.mysqli_error($mysqli));
             if($query){
                 header("Location: ../../main.php?module=producto&alert=3");
